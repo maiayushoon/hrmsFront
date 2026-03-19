65cb5ccd1c5667bdad9c658e9878ddc0f4a02bef
maiayushoon <ayush92002@gmail.com>
2026-03-19 12:40:39 +0530

    pr request

--- src/api/api.ts
+++ src/api/api.ts
@@ -1,7 +1,8 @@
 import axios from "axios";
 
 const API = axios.create({
-  baseURL: "http://localhost:8000/api",
+  baseURL: "https://hrmsback-fvwe.onrender.com/api",
+  // baseURL: "http://localhost:8000/api",
   headers: { "Content-Type": "application/json" },
 });
 
